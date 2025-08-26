846507870feaccd33bb770a8a738f6875549af8f
Bhumika <bhumika04shimla@gmail.com>
2025-08-26 15:04:17 +0530

    Add a product to the cart

--- index.ts
+++ index.ts
@@ -11,3 +11,26 @@ const productList = document.getElementById("product-list") as HTMLElement;
 const cartItemsContainer = document.getElementById("cart-items") as HTMLElement;
 const totalCostElement = document.getElementById("total-cost") as HTMLElement;
 const checkoutButton = document.getElementById("checkout") as HTMLElement;
+
+//Cart array to store items
+let cart: CartItem[] = [];
+
+loadCartFromLocalStorage();
+//add a product to the cart
+function addToCart(productId: string, productName: string, productPrice: number): void {
+    const existingItem = cart.find(item => item.id === productId);
+
+    if(existingItem) {
+        existingItem.quantity++;
+    } else {
+        const newItem: CartItem = {
+            id: productId,
+            name: productName,
+            price: productPrice,
+            quantity: 1
+        };
+        cart.push(newItem);
+    }
+    saveCartToLocalStorage();
+    renderCart();
+}
